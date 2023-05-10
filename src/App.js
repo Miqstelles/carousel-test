d37b3843d40b7ffbb3e16900b3221ad660bc046f
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-05-10 03:32:46 -0300

    slider timeout

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import { useState } from 'react';
 function App() {
   const [index, setIndex] = useState(0)
   let img = carousel[index]
-  function handleClick() {
+
+  setTimeout(() => {
     index === 2 ? setIndex(0) : setIndex(index + 1)
-  }
+  }, 5000)
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={handleClick}>NEXT</button>
+        <button>NEXT</button>
         <img src={img.url} alt="teste" title="teste" />
       </header>
     </div>
